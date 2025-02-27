3f985d455047692ba2be961620719ef11cd2fba8
Osama <osama570ss@gmail.com>
2025-02-26 21:04:58 -0700

    Edit NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,17 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="bg-blue-500 text-white p-4 flex justify-between">
-      <h1 className="text-xl font-bold">Smart Garbage System</h1>
-      <div>
-        <Link to="/" className="mx-4">Dashboard</Link>
-        <Link to="/add" className="mx-4">Add Garbage</Link>
-      </div>
-    </nav>
+    <div class="navbar bg-base-100 bg-neutral text-neutral-content">
+    <div class="flex-1">
+      <a class="btn btn-ghost text-xl">Smart Grabage</a>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal px-1">
+        <li> <Link className="mx-4 px-3 py-2 rounded-md text-white hover:text-gray-300 focus:bg-white focus:text-black active:bg-white active:text-black" to="/">Dashboard</Link></li>
+        <li><Link className="mx-4 px-3 py-2 rounded-md text-white hover:text-gray-300 focus:bg-white focus:text-black active:bg-white active:text-black" to="/add" >Add Garbage</Link></li>
+      </ul>
+    </div>
+  </div>
   );
 };
 
