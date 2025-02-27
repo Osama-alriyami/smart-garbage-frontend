b9218b37636845f7d7d4afb987e22a814fcb507b
Osama <osama570ss@gmail.com>
2025-02-26 20:39:45 -0700

    fix phone view

--- src/components/GarbageList.js
+++ src/components/GarbageList.js
@@ -3,13 +3,16 @@ import GarbageCard from "./GarbageCard";
 
 const GarbageList = ({ garbageData }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
+    <div className="flex justify-center items-center w-full min-h-screen">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
       {garbageData.length > 0 ? (
         garbageData.map((bin) => <GarbageCard key={bin._id} bin={bin} />)
       ) : (
         <p className="text-center col-span-3">No data available</p>
       )}
+      </div>
     </div>
+    
   );
 };
 
